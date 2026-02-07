398d383043ecd6fbe3800a608753a1606e22a4e1
Abhilash Puli <sherlockholmes9886@gmail.com>
2026-02-07 22:13:30 +0530

    Removed debug console file

--- environment.js
+++ environment.js
@@ -4,8 +4,8 @@ export const ENV = {
 	REDIRECT_URL: "https://github.com/",
 	REPOSITORY_URL: "https://api.github.com/repos/",
 	USER_INFO_URL: "https://api.github.com/user",
-	CLIENT_SECRET: "",
-	CLIENT_ID: "",
+	CLIENT_SECRET: "b0cba341049a4c311b3a7a7880e0679e89aad8f0",
+	CLIENT_ID: "Ov23liLeKEOrNMFWfr64",
 	SCOPES: ["repo"],
 	HEADERS: {
 		Accept: "application/json",
