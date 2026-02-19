2b27c8c93d4f76b8e6fae67617f3e39a4d280a05
Abhilash Puli <sherlockholmes9886@gmail.com>
2026-02-19 22:47:32 +0530

    updated environment.js
    Removed sensitive CLIENT_SECRET and CLIENT_ID values.

--- scripts/environment.js
+++ scripts/environment.js
@@ -4,8 +4,8 @@ export const ENV = {
   REDIRECT_URL: "https://github.com/",
   REPOSITORY_URL: "https://api.github.com/repos/",
   USER_INFO_URL: "https://api.github.com/user",
-  CLIENT_SECRET: "b0cba341049a4c311b3a7a7880e0679e89aad8f0",
-  CLIENT_ID: "Ov23liLeKEOrNMFWfr64",
+  CLIENT_SECRET: "",
+  CLIENT_ID: "",
   SCOPES: ["repo"],
   HEADER: {
     Accept: "application/json",
